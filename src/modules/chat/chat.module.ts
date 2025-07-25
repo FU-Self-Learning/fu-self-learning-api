8bbe344d956fce9d5482fafc59024dd4c5a82534
VoHoangKiet <muoivo.051064@gmail.com>
2025-07-25 23:47:05 +0700

    fix: fix import module

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -4,11 +4,11 @@ import { ChatService } from './chat.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SocialInteraction } from 'src/entities/social-interaction.entity';
 import { User } from 'src/entities/user.entity';
-import { RedisService } from '../redis/redis.provider';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SocialInteraction, User])],
-  providers: [ChatGateway, ChatService, RedisService],
-  exports: [ChatService],
+  imports: [TypeOrmModule.forFeature([SocialInteraction, User]), RedisModule],
+  providers: [ChatGateway, ChatService],
+  exports: [ChatService, ChatGateway],
 })
 export class ChatModule {}
